a21a22c8408eb8cb8debf5869045a11b530318af
Nastusha228 <logashina84@gmail.com>
2024-11-19 23:21:31 +0200

    Add files via upload

---
+++ les2.js
@@ -0,0 +1,29 @@
+{
+    const name = "Анастасия";
+
+    let age = "40";
+    let isStudent = "ЦОПП";
+    const height = "175см";
+    let hobby = "рисование";
+    const favoriteNumber = "7";
+    const isGraduated = "КГТУ";
+
+    console.log(name, age, isStudent, height, hobby, favoriteNumber, isGraduated); 
+
+    let work;
+    let address = undefined;
+    let weight = null;
+    let line = `${work} || ${address} || ${weight}`
+    console.log(line);
+
+    const birth = "1984";
+    let now = "2024";
+    let ages = `Привет, мне ${now - birth} лет`;
+    console.log(ages);
+
+
+    
+    let year = 2024
+    let newDate = year - birth
+    console.log(newDate);
+}
